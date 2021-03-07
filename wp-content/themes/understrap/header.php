7b76fde584df9592717754a794a6d7c31de69aa7
Katie Rose Felten <katiefelten@gmail.com>
2021-03-06 19:45:04 -0500

    :tada: some tings

--- wp-content/themes/understrap/header.php
+++ wp-content/themes/understrap/header.php
@@ -14,40 +14,55 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
-<?php do_action( 'wp_body_open' ); ?>
-<div class="site" id="page">
+    <?php do_action( 'wp_body_open' ); ?>
+    <div class="site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
+        <!-- ******************* The Navbar Area ******************* -->
 
-	<div id="top-bar">
-		<div class="contact-info">
-			<div class="phone">
-			<h1>Hello, world</h1>
-			</div>
-		</div>
-	</div> 
-	<div id="wrapper-navbar">
+        <div id="top-bar">
+            <div class="contact-info">
+                <div class="contact-item phone">
+                    <div class="phone">
+                        <i class="fa fa-phone">
+                            420-614-8004
+                        </i>
+                    </div>
+                </div>
+                <div class="contact-item email">
+                    <div class="email">
+                        <i class="fa fa-phone">
+                            hello@mailman.com
+                        </i>
+                    </div>
+                </div>
+                <div class="search"></div>
+            </div>
+        </div>
+        <div id="wrapper-navbar">
 
-		<nav id="main-nav" class="navbar navbar-expand-lg" aria-labelledby="main-nav-label">
+            <nav id="main-nav" class="navbar navbar-expand-lg" aria-labelledby="main-nav-label">
 
-			<h2 id="main-nav-label" class="sr-only">
-				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-			</h2>
+                <h2 id="main-nav-label" class="sr-only">
+                    <?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+                </h2>
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-				<!-- The WordPress Menu goes here -->
-				<?php
+                <!-- The WordPress Menu goes here -->
+                <?php
 				wp_nav_menu(
 					array(
 						'theme_location'  => 'primary',
@@ -62,6 +77,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 				);
 				?>
 
-		</nav><!-- .site-navigation -->
+            </nav><!-- .site-navigation -->
 
-	</div><!-- #wrapper-navbar end -->
+        </div><!-- #wrapper-navbar end -->
