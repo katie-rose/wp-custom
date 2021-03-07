69332025e099ff1f4bd19521ab89f6285dbb1240
Katie Rose Felten <katiefelten@gmail.com>
2021-03-07 10:27:34 -0500

    clean up

--- wp-content/themes/understrap/header.php
+++ wp-content/themes/understrap/header.php
@@ -18,7 +18,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;400;500;700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
