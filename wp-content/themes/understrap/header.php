5c765a57174fd9a40ef1c1572d100b41fc2a90ce
Katie Rose Felten <katiefelten@gmail.com>
2021-03-07 17:46:45 -0500

    wut

--- wp-content/themes/understrap/header.php
+++ wp-content/themes/understrap/header.php
@@ -19,7 +19,8 @@ $container = get_theme_mod( 'understrap_container_type' );
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;400;500;700&display=swap"
+        rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital@0;1&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
