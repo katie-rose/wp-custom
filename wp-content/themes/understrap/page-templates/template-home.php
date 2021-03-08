aa7f9f045beafecf7cc6aa2ca65b22bbf86618f4
Katie Rose Felten <katiefelten@gmail.com>
2021-03-07 19:08:31 -0500

    :zap: three column photos added

--- wp-content/themes/understrap/page-templates/template-home.php
+++ wp-content/themes/understrap/page-templates/template-home.php
@@ -18,9 +18,40 @@ get_header();
         A community dedicated to the everyday adventurer, photo taker and story maker. Photo printing, photo
         scanning and beyond.
     </h2>
-    <h3>No story untold</h3>
+    <h3>No story untold.</h3>
 </section>
 
+<section class="home-call-to-action">
+    <div class="row">
+        <div class="col-lg-4">
+            <a href="#" class="action">
+                <div class="title">Fine Art Prints</div>
+                <div class="img"></div>
+            </a>
+        </div>
+
+        <div class="col-lg-4">
+            <a href="#" class="action">
+                <div class="title">Every Day Photo Prints</div>
+                <div class="img"
+                    style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/photo-2.jpg);">
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-4">
+            <a href="#" class="action">
+                <div class="title">Beyond Photography</div>
+                <div class="img"
+                    style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/photo-3.jpg);">
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
+
+<section class="home-about">
+</section>
 
 <?php
 get_footer();
